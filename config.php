da6b6db4c94927086d57eebf8f5f0fa4e5ab7809
Muhammad Adamu Garba <58890329+MW-Tech-Solutions@users.noreply.github.com>
2026-07-16 00:38:08 +0100

    Remove SMTP configuration from config.php
    Removed SMTP mailer configuration settings.

--- config.php
+++ config.php
@@ -19,19 +19,5 @@ return [
         'max_records' => 10000,
     ],
     'mail' => [
-        'mailer' => 'smtp',
-        'host' => 'smtp.gmail.com',
-        'port' => 587,
-        'username' => 'jostum.services@uam.edu.ng',
-        'password' => 'yzkwkpubirsdtcoe',
-        'encryption' => 'tls',
-        'from_name' => 'JOSTUM ID CARD COMMITTEE',
-        'from_email' => 'jostum.services@uam.edu.ng',
-        'batch_size' => 4,
-        'batch_interval_seconds' => 120,
-        'max_retries' => 3,
-        'retry_delay_seconds' => 300,
-        'send_delay_microseconds' => 350000,
-        'php_binary' => 'C:\\xampp\\php\\php.exe',
-    ],
+       ]
 ];
